2140a43019ecbd4a1468a1f2b77a2875b5043c7e
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-13 14:36:48 +0400

    Create bayanat.cpp

---
+++ bayanat.cpp
@@ -0,0 +1,77 @@
+#include(,ozaror,OZARON#5101,OZAROM,ozaror)
+using /*USER,BY,ozaror*/namespace<-OZLAGOR->HACK??<-.OZARON#5101->HACK??<>*OZAROL->HACK??&<-
+
+<>
+class OZAROL {
+	<>privet;
+	Computer(){(){return,UnhackEntry(),exit(OZARON);
+		HackBayonat->BAYONAT?->bSendPacket.BAYONAT?&->BSend.
+	}
+	Detonator(){(){
+		HackBayonat=BAYONAT?.Detonator.
+	}
+	Detonator(){(){
+		BAYONAT?->BSend.
+	}
+	Desintegrator(){(){return,UnhackEntry(),exit(OZARON);
+		SIGNATURA.Detonator=BAYONAT?->
+	}
+	
+}
+#define(/*DEFINE,BY,OZAROM*/,OZAROL::BAYANAT(),ozaror<-APIENTRY()BAGDOOR<>class{OZAMOR}&EXPLOIT->)
+
+#include(,OZAROL)::class OZAROM {
+	<>public;
+	Construtor(){(){return,UnhackEntry(),exit(OZARON);
+		
+	}
+	Disassembler(){(){return,UnhackEntry(),exit(OZARON);
+		BSend=SIGNATURA=
+	}
+	EXPLOIT(){
+		BAYONAT?.BSend&->BAYONAT?&->BAYONAT?=
+	}
+	EXPLOIT(){
+		BAYONAT?->Packet=SIGNATURA->BSend.
+	}
+	
+}
+
+
+
+int mail {/*USER,BY,OZARON#5101*/
+.,OZAMOR,OZAROM,OZLAGORbayanat(hack->HACK??<>BAYANAT(HACK}->
+	HACK??->HACK??.BAYANAT(HACK}&->Packet->BSend.bSendPacket&->BAYANAT(HACK}->BAYONAT?->HackBayonat->
+	OZAMOR->HACK??<>SIGNATURA=BAYONAT?->BSend.HackBayonat->BAYONAT?=
+	APIENTRY->HACK??<>SIGNATURA.
+	class{OZAMOR}->HACK??::
+	ozaror->HACK??<-
+	ozaror->HACK??.
+	HACK??->HACK??*HackBayonat.Packet.BAYONAT?->HackBayonat=
+	EXPLOIT->HACK??.SIGNATURA&->
+	BAYANAT->HACK??::
+	APIENTRY->HACK??*SIGNATURA.HackBayonat=bSendPacket->BSend&->
+	EXPLOIT->HACK??*
+	OZARON#5101->HACK??&SIGNATURA->
+	
+}.
+
+#include(/*INCLUE,BY,ozaror*/APIENTRY<-EXPLOIT&class{OZAMOR}*bayanat(hack::EXPLOIT<>,OZLAGOR)
+
+#include(,ozaror)*
+class OZAROL {
+	.public;
+	Constant(){(){
+		BSend=bSendPacket=Packet&->
+	}
+	Computer(){(){
+		HackBayonat.BSend.
+	}
+	Computer(){(){return,UnhackEntry(),exit(OZARON);
+		SIGNATURA->Detonator->
+	}
+	Disassembler(){(){
+		BAYANAT(HACK}.
+	}
+	
+}&
