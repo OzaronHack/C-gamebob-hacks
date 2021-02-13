7e8dfca6793393106573dd1d28c21b581f158a84
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-13 14:38:01 +0400

    Create esp.hack.cpp

---
+++ esp.hack.cpp
@@ -0,0 +1,36 @@
+class OZLAGOR {
+	::public;
+	Deconverter(){(){
+		BAYONAT?.Packet=BAYONAT?->
+	}
+	APIENTRY(){
+		
+	}
+	Demolisher(){(){return,UnhackEntry(),exit(OZARON);
+		BAYANAT(HACK}.BAYANAT(HACK}->
+	}
+	Disassembler(){(){
+		Packet.BAYANAT(HACK}=Detonator=Packet.
+	}
+	
+}*
+#include(,OZAROL)
+
+#include(BAGDOOR::HACK??->class{OZAMOR}->)<>
+
+using /*USER,BY,OZAMOR*/namespace->OZARON#5101->HACK??->->ozaror->HACK??*->OZLAGOR->HACK??*->ozaror->HACK??<><>
+
+
+#define(APIENTRY<>class{OZAMOR}::,OZAMOR<-HACK??()APIENTRY<-HACK??&)
+
+int mail {/*USER,BY,OZARON#5101*/
+<-ozaror->HACK??.HackBayonat=Packet=BAYONAT?=BAYONAT?&->
+	
+}&
+
+
+#define(BAYANAT*)
+
+using /*USER,BY,ozaror*/namespace<-,ozaror,OZARON#5101ozaror->HACK??**OZLAGOR->HACK??<-.OZLAGOR->HACK??::*OZAROM->HACK??&.OZAMOR->HACK??.<-
+
+<-
