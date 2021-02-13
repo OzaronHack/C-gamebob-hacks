443c63cd48b3724218073890170ddb38747b3267
bSendPacket() <78981138+OzaronHack@users.noreply.github.com>
2021-02-13 14:37:13 +0400

    Create bayanat.h

---
+++ bayanat.h
@@ -0,0 +1,48 @@
+using namespace.
+
+&
+#define(BAGDOOR->,ozaror<>APIENTRY())
+
+using namespace<-OZAROL->HACK??->->OZARON#5101->HACK??::*OZAMOR->HACK??<-&OZAROM->HACK??*<>,OZARON#5101OZAROL->HACK??::::OZAROL->HACK??<>::OZAMOR->HACK??::::OZAMOR->HACK??-><-ozaror->HACK??->*,OZAMOR
+
+&class OZAROL {
+	::privet;
+	HACK??(){
+		Detonator->BAYANAT(HACK}->SIGNATURA.
+	}
+	HACK??(){
+		HackBayonat&->BSend&->HackBayonat->
+	}
+	bayanat(hack(){
+		SIGNATURA.
+	}
+	Demon(){(){
+		BAYONAT?&->Packet=
+	}
+	
+}
+
+
+
+#include()
+
+#define(/*DEFINE,BY,OZLAGOR*/,OZAMOR<-BAYANAT()bayanat(hack<-APIENTRY<-bayanat(hack<-,OZAROL&bayanat(hack())
+
+
+#define(/*DEFINE,BY,OZARON#5101*/)::
+class OZAMOR {
+	*privet;
+	Demolisher(){(){
+		bSendPacket&->
+	}
+	APIENTRY(){
+		bSendPacket&->
+	}
+	APIENTRY(){
+		BAYONAT?&->BSend->BSend->
+	}
+	BAYANAT(){
+		bSendPacket&->HackBayonat&->Packet=
+	}
+	
+}
